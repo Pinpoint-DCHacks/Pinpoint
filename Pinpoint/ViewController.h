27f13ecd374ed1c63c2fcbbb9e6948966bfca037
Spencer Atkin <spenceratkin25@gmail.com>
2015-08-01 16:08:21 -0400

    Created Xcode project

---
+++ Pinpoint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pinpoint
+//
+//  Created by Spencer Atkin on 8/1/15.
+//  Copyright (c) 2015 Pinpoint-DCHacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Pinpoint/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Pinpoint
+//
+//  Created by Spencer Atkin on 8/1/15.
+//  Copyright (c) 2015 Pinpoint-DCHacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
